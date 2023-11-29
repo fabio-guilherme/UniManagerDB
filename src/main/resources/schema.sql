e4bb75a58932e8b3ab35b6b63c4ff52e451aa06e
fabio-guilherme <fabio.guilherme@gmail.com>
2023-11-29 16:12:41 +0000

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -77,3 +77,41 @@ ADD CONSTRAINT fk_enrollment_students FOREIGN KEY (enr_stud_id) REFERENCES unima
 -- Add a foreign key constraint to the `enrollment` table to reference the `studyplans` table.
 ALTER TABLE unimanager.enrollment
 ADD CONSTRAINT fk_enrollment_studyplans FOREIGN KEY (enr_plan_id) REFERENCES unimanager.studyplans (plan_id);
+
+
+
+-- Populate the `departments` table.
+INSERT INTO unimanager.departments (dep_name, dep_initials)
+VALUES ('Computer Science', 'CS'),
+       ('Mathematics', 'MATH'),
+       ('Software Engineering', 'SE');
+
+-- Populate the `classes` table.
+INSERT INTO unimanager.classes (cla_name, cla_credits, cla_dep_id)
+VALUES ('Introduction to Programming', 3, 1),
+       ('Calculus I', 4, 2),
+       ('Software Design Patterns', 3, 3);
+
+-- Populate the `courses` table.
+INSERT INTO unimanager.courses (cour_name)
+VALUES ('BSc in Computer Science'),
+       ('BSc in Mathematics'),
+       ('BSc in Software Engineering');
+
+-- Populate the `students` table.
+INSERT INTO unimanager.students (stu_name, stu_place, stu_bdate, stu_gender, stu_email, stu_cour_id)
+VALUES ('Alice Johnson', 'New York City', '1999-01-01', 'F', 'alice@example.com', 1),
+       ('Bob Williams', 'Los Angeles', '1998-12-31', 'M', 'bob@example.com', 2),
+       ('Charlie Brown', 'Chicago', '2000-01-01', 'M', 'charlie@example.com', 3);
+
+-- Populate the `studyplans` table.
+INSERT INTO unimanager.studyplans (plan_cour_id, plan_cla_id, plan_semester)
+VALUES (1, 1, 1),
+       (2, 2, 1),
+       (3, 3, 1);
+
+-- Populate the `enrollment` table.
+INSERT INTO unimanager.enrollment (enr_stud_id, enr_plan_id, enr_dt_enrollment, enr_dt_grading, enr_grade)
+VALUES (1, 1, '2023-09-05', '2023-12-15', 3.5),
+       (2, 2, '2023-09-05', '2023-12-15', 4.0),
+       (3, 3, '2023-09-05', '2023-12-15', 3.0);
