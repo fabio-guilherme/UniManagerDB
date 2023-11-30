00f39a0601803841bde32ca102ad52b0d3a51bab
fabio-guilherme <fabio.guilherme@gmail.com>
2023-11-30 17:05:14 +0000

    Update Unit.java

--- src/main/java/pt/iade/unimanagerdb/models/Unit.java
+++ src/main/java/pt/iade/unimanagerdb/models/Unit.java
@@ -1,27 +1,37 @@
 package pt.iade.unimanagerdb.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@Entity 
-@Table(name="classes")
+@Entity
+@Table(name = "classes")
 public class Unit {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="cla_id")    private int id;
-    @Column(name="cla_name")  private String name;
-    @Column(name="cla_credits") private int credits;
-    public Unit() {}
-    public int getId() { return id; }
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cla_id")
+    private int id;
+    @Column(name = "cla_name")
+    private String name;
+    @Column(name = "cla_credits")
+    private int credits;
+
+    public Unit() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
+
     public int getCredits() {
         return credits;
     }
-    
+
 }
